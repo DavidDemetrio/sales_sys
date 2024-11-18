727126f5f888aa4897b36f5e0819bd4c36a19136
DavidDemetrio <davidlopez.paz26@outlook.es>
2024-11-18 00:41:11 -0600

    first commit

---
+++ src/sales/Sales.java
@@ -0,0 +1,10 @@
+package sales;
+
+public class Sales {
+    public static void main(String[] args) {
+        System.out.println("*** Sales System ***");
+
+        var t_Shirt = new Product("T-Shirt", 123);
+        System.out.println(t_Shirt);
+    }
+}
